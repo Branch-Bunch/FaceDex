db64d1e4dec46bfc806b32ebe0599245353afc89
BenEmdon <benjaminemdon@gmail.com>
2017-02-04 02:29:08 -0500

    Added project file with SwiftyCam

---
+++ FaceDex/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FaceDex
+//
+//  Created by Benjamin Emdon on 2017-02-04.
+//  Copyright © 2017 Benjamin Emdon. All rights reserved.
+//
+
+import UIKit
+import SwiftyCam
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = UIColor.white
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ Pods/Target Support Files/Pods-FaceDex/Pods-FaceDex-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FaceDex : NSObject
+@end
+@implementation PodsDummy_Pods_FaceDex
+@end
