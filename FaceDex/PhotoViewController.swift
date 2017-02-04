953feb67b0413a3369333d7c311a99829f4d967c
BenEmdon <benjaminemdon@gmail.com>
2017-02-04 10:05:30 -0500

    added swiftyCam UI

---
+++ FaceDex/PhotoViewController.swift
@@ -0,0 +1,44 @@
+//
+//  PhotoViewController.swift
+//  FaceDex
+//
+//  Created by Benjamin Emdon on 2017-02-04.
+//  Copyright © 2017 Benjamin Emdon. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+	override var prefersStatusBarHidden: Bool {
+		return true
+	}
+
+	private var backgroundImage: UIImage
+
+	init(image: UIImage) {
+		self.backgroundImage = image
+		super.init(nibName: nil, bundle: nil)
+		// make image request
+	}
+
+	required init?(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		self.view.backgroundColor = UIColor.gray
+		let backgroundImageView = UIImageView(frame: view.frame)
+		backgroundImageView.image = backgroundImage
+		view.addSubview(backgroundImageView)
+		let cancelButton = UIButton(frame: CGRect(x: 10.0, y: 10.0, width: 30.0, height: 30.0))
+		cancelButton.setImage(#imageLiteral(resourceName: "cancel"), for: UIControlState())
+		cancelButton.addTarget(self, action: #selector(cancel), for: .touchUpInside)
+		view.addSubview(cancelButton)
+	}
+
+	func cancel() {
+		dismiss(animated: true, completion: nil)
+	}
+}
--- FaceDex/ViewController.swift
+++ FaceDex/ViewController.swift
@@ -9,19 +9,113 @@
 import UIKit
 import SwiftyCam
 
-class ViewController: UIViewController {
+class ViewController: SwiftyCamViewController, SwiftyCamViewControllerDelegate {
+
+	var flipCameraButton: UIButton!
+	var flashButton: UIButton!
+	var captureButton: SwiftyRecordButton!
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		view.backgroundColor = UIColor.white
-		// Do any additional setup after loading the view, typically from a nib.
+		cameraDelegate = self
+		maximumVideoDuration = 10.0
+		addButtons()
 	}
 
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	override var prefersStatusBarHidden: Bool {
+		return true
 	}
 
+	override func viewDidAppear(_ animated: Bool) {
+		super.viewDidAppear(animated)
+	}
 
+	func swiftyCam(_ swiftyCam: SwiftyCamViewController, didTake photo: UIImage) {
+		let newVC = PhotoViewController(image: photo)
+		self.present(newVC, animated: true, completion: nil)
+	}
+
+	func swiftyCam(_ swiftyCam: SwiftyCamViewController, didBeginRecordingVideo camera: SwiftyCamViewController.CameraSelection) {
+		print("Did Begin Recording")
+		captureButton.growButton()
+		UIView.animate(withDuration: 0.25, animations: {
+			self.flashButton.alpha = 0.0
+			self.flipCameraButton.alpha = 0.0
+		})
+	}
+
+	func swiftyCam(_ swiftyCam: SwiftyCamViewController, didFinishRecordingVideo camera: SwiftyCamViewController.CameraSelection) {
+		print("Did finish Recording")
+		captureButton.shrinkButton()
+		UIView.animate(withDuration: 0.25, animations: {
+			self.flashButton.alpha = 1.0
+			self.flipCameraButton.alpha = 1.0
+		})
+	}
+
+//	func swiftyCam(_ swiftyCam: SwiftyCamViewController, didFinishProcessVideoAt url: URL) {
+//		let newVC = VideoViewController(videoURL: url)
+//		self.present(newVC, animated: true, completion: nil)
+//	}
+
+	func swiftyCam(_ swiftyCam: SwiftyCamViewController, didFocusAtPoint point: CGPoint) {
+		let focusView = UIImageView(image: #imageLiteral(resourceName: "focus"))
+		focusView.center = point
+		focusView.alpha = 0.0
+		view.addSubview(focusView)
+
+		UIView.animate(withDuration: 0.25, delay: 0.0, options: .curveEaseInOut, animations: {
+			focusView.alpha = 1.0
+			focusView.transform = CGAffineTransform(scaleX: 1.25, y: 1.25)
+		}, completion: { (success) in
+			UIView.animate(withDuration: 0.15, delay: 0.5, options: .curveEaseInOut, animations: {
+				focusView.alpha = 0.0
+				focusView.transform = CGAffineTransform(translationX: 0.6, y: 0.6)
+			}, completion: { (success) in
+				focusView.removeFromSuperview()
+			})
+		})
+	}
+
+	func swiftyCam(_ swiftyCam: SwiftyCamViewController, didChangeZoomLevel zoom: CGFloat) {
+		print(zoom)
+	}
+
+	func swiftyCam(_ swiftyCam: SwiftyCamViewController, didSwitchCameras camera: SwiftyCamViewController.CameraSelection) {
+		print(camera)
+	}
+
+	@objc private func cameraSwitchAction(_ sender: Any) {
+		switchCamera()
+	}
+
+	@objc private func toggleFlashAction(_ sender: Any) {
+		flashEnabled = !flashEnabled
+
+		if flashEnabled == true {
+			flashButton.setImage(#imageLiteral(resourceName: "flash"), for: UIControlState())
+		} else {
+			flashButton.setImage(#imageLiteral(resourceName: "flashOutline"), for: UIControlState())
+		}
+	}
+
+	private func addButtons() {
+		captureButton = SwiftyRecordButton(frame: CGRect(x: view.frame.midX - 37.5, y: view.frame.height - 100.0, width: 75.0, height: 75.0))
+		self.view.addSubview(captureButton)
+		captureButton.delegate = self
+
+		flipCameraButton = UIButton(frame: CGRect(x: (((view.frame.width / 2 - 37.5) / 2) - 15.0), y: view.frame.height - 74.0, width: 30.0, height: 23.0))
+		flipCameraButton.setImage(#imageLiteral(resourceName: "flipCamera"), for: UIControlState())
+		flipCameraButton.addTarget(self, action: #selector(cameraSwitchAction(_:)), for: .touchUpInside)
+		self.view.addSubview(flipCameraButton)
+
+		let test = CGFloat((view.frame.width - (view.frame.width / 2 + 37.5)) + ((view.frame.width / 2) - 37.5) - 9.0)
+
+		flashButton = UIButton(frame: CGRect(x: test, y: view.frame.height - 77.5, width: 18.0, height: 30.0))
+		flashButton.setImage(#imageLiteral(resourceName: "flashOutline"), for: UIControlState())
+		flashButton.addTarget(self, action: #selector(toggleFlashAction(_:)), for: .touchUpInside)
+		self.view.addSubview(flashButton)
+	}
 }
+
 
